991a9cc39151382d3462b272c9f3ca33d0b27add
sa94my <ahmad_samy93@yahoo.com>
2021-11-06 15:20:34 +0200

    add screenshot

--- src/test/java/herokuApp_tests/HerokuTestBase.java
+++ src/test/java/herokuApp_tests/HerokuTestBase.java
@@ -2,13 +2,19 @@ package herokuApp_tests;
 
 import herokuappPages.HerokuPageBase;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class HerokuTestBase {
@@ -27,8 +33,9 @@ public class HerokuTestBase {
     }
 
     @AfterMethod
-    public void tearDown(){
-    driver.quit();
+    public void tearDown(ITestResult testResult){
+        Allure.addAttachment(testResult.getName(), new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        driver.quit();
     }
 
     @AfterSuite
