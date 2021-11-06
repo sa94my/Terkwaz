39d3b9ddee400c271fdacbb77086397c76bb084a
sa94my <ahmad_samy93@yahoo.com>
2021-11-06 23:29:56 +0200

    safety net

--- src/test/java/catFacts_API/getOneRandomFact.java
+++ src/test/java/catFacts_API/getOneRandomFact.java
@@ -4,18 +4,16 @@ import io.qameta.allure.Allure;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.Test;
 
-import java.io.ByteArrayInputStream;
 
 public class getOneRandomFact {
-
+    String baseUrl="https://cat-fact.herokuapp.com/facts";
+    String randomEndPoint ="/random";
     @Test(description = "Ensure request return a cat fact as text")
-    public void getFact(){
+    public void validateTextNotEmpty(){
        Response response = RestAssured.given().
-                when().get("https://cat-fact.herokuapp.com/facts/random")
+                when().get(baseUrl+randomEndPoint)
                 .then().
                 assertThat().body("text",Matchers.not(Matchers.emptyOrNullString())).extract().response();
         Allure.addAttachment("Response Body", response.jsonPath().prettify());
